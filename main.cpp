dc9380668b649dfb7a2f64cca14196fa836dc90c
Andrey <vikakaef@inbox.ru>
2024-12-21 20:10:33 +0500

    End of 3rd and 4th task

--- main.cpp
+++ main.cpp
@@ -20,9 +20,22 @@ uint64_t fileLink;
 uint32_t nextIdent;
 uint32_t numFileRec;
 };
-
+struct ATRHeader {
+uint32_t type;
+uint32_t atrLength;
+char flag;
+char length;
+uint16_t nameOffsetAttr;
+uint16_t packFlag;
+uint16_t atrIdent;
+uint32_t atrSize;
+uint16_t conOffset;
+char flagIndex;
+char atrFill;
+};
 int main()
 {
+    setlocale(LC_ALL,"Russian");
     ifstream ntfs_file("File_record.dat",ios::binary);
     if (ntfs_file.is_open()) {
     ntfs_file.seekg(0,ios::end);
@@ -33,6 +46,55 @@ int main()
     NTFSHeader* p_header = reinterpret_cast<NTFSHeader*>(&ntfs_data[0]);
     cout << hex << int(p_header ->fileIdent) << endl;;
     cout << hex << int(p_header ->firstOffset) << endl;;
+    cout << hex << int(p_header ->numFileRec) << endl;
+    int offset = int(p_header->firstOffset);
+    while(true) {
+        ATRHeader* pa_header = reinterpret_cast<ATRHeader*>(&ntfs_data[offset]);
+        if ((pa_header->type) == 0xFFFFFFFF) {
+            break;
+        }
+        int TypeAtrib = pa_header->type;
+        cout << hex << "Type of attribute: " << TypeAtrib;
+        switch(TypeAtrib) {
+    case 0x10:
+        cout <<" - $STANDART_INFORMATION"<<endl;
+        break;
+    case 0x20:
+        cout <<" - $ATTRIBUTE_LIST"<<endl;
+        break;
+    case 0x30:
+        cout <<" - $FILE_NAME"<<endl;
+        break;
+    case 0x40:
+        cout <<" - $OBJECT_ID"<<endl;
+        break;
+    case 0x50:
+        cout <<" - $SECURITY_DESCRIPTOR"<<endl;
+        break;
+    case 0x80:
+        cout <<" - $DATA"<<endl;
+        break;
+    case 0x90:
+        cout <<" - $INDEX_ROOT"<<endl;
+        break;
+    case 0xA0:
+        cout <<" - $INDEX_ALLOCATION"<<endl;
+        break;
+    case 0xB0:
+        cout <<" - $BITMAP"<<endl;
+        break;
+    case 0x100:
+        cout <<" - $LOGGED_UTILITY_STREAM"<<endl;
+        break;
+        }
+        if (TypeAtrib == 0x30) {
+           int sizeOfName = ntfs_data[offset+88];
+           wchar_t* data = reinterpret_cast<wchar_t*>(&ntfs_data[offset+90]);
+           wstring fileName = wstring(data, sizeOfName);
+           wcout << fileName << endl;
+        }
+        offset +=pa_header->atrLength;
+    }
 
 
     }
